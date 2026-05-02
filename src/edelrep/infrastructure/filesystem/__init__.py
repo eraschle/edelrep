22efd2416cc49c911948e646a061b033e997a89b
Erich Raschle <erichraschle@gmail.com>
2026-05-02 19:41:26 +0200

    feat(infra): re-export filesystem repositories

--- src/edelrep/infrastructure/filesystem/__init__.py
+++ src/edelrep/infrastructure/filesystem/__init__.py
@@ -1,1 +1,9 @@
+from edelrep.infrastructure.filesystem.image_store import FilesystemImageRepository
+from edelrep.infrastructure.filesystem.repair_store import FilesystemRepairRepository
+from edelrep.infrastructure.filesystem.vehicle_store import FilesystemVehicleRepository
 
+__all__ = [
+    "FilesystemImageRepository",
+    "FilesystemRepairRepository",
+    "FilesystemVehicleRepository",
+]
