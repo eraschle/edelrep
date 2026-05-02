75f1e68b00c9d5001e8f48407145bca1df9dd9ff
Erich Raschle <erichraschle@gmail.com>
2026-05-02 18:46:24 +0200

    feat(domain): re-export ports from edelrep.domain.ports

--- src/edelrep/domain/ports/__init__.py
+++ src/edelrep/domain/ports/__init__.py
@@ -1,1 +1,17 @@
+from edelrep.domain.ports.email_inbox import EmailAttachment, EmailInbox, EmailMessage
+from edelrep.domain.ports.image_repository import ImageRepository
+from edelrep.domain.ports.repair_repository import RepairRepository
+from edelrep.domain.ports.search_index import SearchIndex
+from edelrep.domain.ports.storage_backend import StorageBackend
+from edelrep.domain.ports.vehicle_repository import VehicleRepository
 
+__all__ = [
+    "EmailAttachment",
+    "EmailInbox",
+    "EmailMessage",
+    "ImageRepository",
+    "RepairRepository",
+    "SearchIndex",
+    "StorageBackend",
+    "VehicleRepository",
+]
